98fb4beeb511d79b0333f3822c90658376e4d7f1
jyyoo0530 <jyyoo0530@gmail.com>
2021-03-23 00:19:27 +0900

    metallb

--- 10_metallb.sh
+++ 10_metallb.sh
@@ -1,6 +1,6 @@
 kubctl create namespace kube-gateway
 
 # On first install only
-kubectl create secret generic -n kube-gateway memberlist --from-literal=secretkey="$(openssl rand -base64 128)" --create-namespace
+kubectl create secret generic -n kube-gateway memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 
 # add app on ArgoCD
