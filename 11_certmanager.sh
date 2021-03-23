ded4c295b04bf2cdd19bc43b936ce7b4b619b87d
jyyoo0530 <jyyoo0530@gmail.com>
2021-03-23 17:05:14 +0900

    postgre

--- 11_certmanager.sh
+++ 11_certmanager.sh
@@ -1,4 +1,15 @@
 kubectl create namespace kube-security
+
+# Generate a CA private key
+openssl genrsa -out ca.key 2048
+
+# Create a self signed Certificate, valid for 10yrs with the 'signing' option set
+openssl req -x509 -new -nodes -key ca.key -subj "/C=KR/ST=Seoul/L=Seoul/O=example/OU=Personal/CN=30.0.2.30" -days 3650 -reqexts v3_req -extensions v3_ca -out ca.crt
+kubectl create secret tls ca-key-pair \
+   --cert=ca.crt \
+   --key=ca.key \
+   --namespace=kube-security
+
 # apply crd by Argo
 # run below
 kubectl label namespace kube-security certmanager.k8s.io/disable-validation=true
