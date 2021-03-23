b8899abd71a663a4d0b9cf45e25526a03fd4a399
jyyoo0530 <jyyoo0530@gmail.com>
2021-03-23 12:59:00 +0900

    postgre

--- 10_metallb.sh
+++ 10_metallb.sh
@@ -1,4 +1,4 @@
-kubctl create namespace kube-gateway
+kubectl create namespace kube-gateway
 
 # On first install only
 kubectl create secret generic -n kube-gateway memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
